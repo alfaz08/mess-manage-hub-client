d28b1be5639a8a5925e2cec2eeede2ba885a1776
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-02 14:45:04 +0600

    user deleted and make admin is added for admin

--- src/pages/Dashboard/Admin/UserList/AllUsers.jsx
+++ src/pages/Dashboard/Admin/UserList/AllUsers.jsx
@@ -1,13 +1,113 @@
 import React from 'react';
 import useAllUsers from '../../../../hooks/useAllUsers';
-
+import { FaUser, FaUserShield } from "react-icons/fa6";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 const AllUsers = () => {
-  const [allUser]=useAllUsers()
+  const [allUser,refetch]=useAllUsers()
+  const axiosSecure =useAxiosSecure()
   console.log(allUser);
+
+  const handleMakeAdmin = user =>{
+    console.log(user);
+     axiosSecure.patch(`/users/admin/${user._id}`)
+     .then(res=>{
+      console.log(res.data);
+      if(res.data.modifiedCount>0){
+        refetch()
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 500,
+        });
+      }
+     })
+  
+   }
+
+
+
+
+  const handleDelete = (email) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${email}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted",
+              text: "Your post has been deleted",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
+
+
+
+
   return (
     <div>
-      
+    <div className="flex justify-evenly my-4">
+      <h2>User List: {allUser?.length}</h2>
     </div>
+    <div className="overflow-x-auto">
+      <table className="table table-zebra ">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>#</th>
+            <td>Name</td>
+            <td>Roll</td>
+            <td>Email</td>
+            <td>Make Admin</td>
+            <td>Delete</td>
+          </tr>
+        </thead>
+        <tbody>
+          {allUser?.map((user, index) => (
+            <tr key={user._id}>
+              <th>{index + 1}</th>
+              <td>{user.name}</td>
+
+              <td>{user.roll}</td>
+              <td>{user.email}</td>
+
+              <td>
+              {user.roll ==='admin' ? 'Admin' :<button
+           onClick={()=>handleMakeAdmin(user)}
+           className="btn bg-orange-500 btn-lg">
+         <FaUser className="text-white text-2xl"></FaUser>
+          </button>}
+              </td>
+              <th>
+                <button
+                  onClick={() => handleDelete(user?.email)}
+                  className="btn btn-ghost btn-lg"
+                >
+                  <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                </button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
   );
 };
 
