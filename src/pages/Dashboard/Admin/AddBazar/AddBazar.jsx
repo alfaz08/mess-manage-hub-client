fcb1acd9c2d7569d872e9c1fb043eda7f2ec4c96
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-15 00:46:21 +0600

    add bazar react from is properly added without functionality

--- src/pages/Dashboard/Admin/AddBazar/AddBazar.jsx
+++ src/pages/Dashboard/Admin/AddBazar/AddBazar.jsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import useAxiosPublic from "../../../../hooks/useAxiosPublic";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import useAuth from "../../../../hooks/useAuth";
@@ -6,54 +6,51 @@ import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../SectionTitle/SectionTitle";
 import Swal from "sweetalert2";
 
-
-
-
 const AddBazar = () => {
-  const axiosPublic =useAxiosPublic()
-  const axiosSecure = useAxiosSecure()
-  const {user} =useAuth()
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
 
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm();
+    control,
+  } = useForm({
+    defaultValues: {
+      items: [{ one: "", item: "" }],
+    },
+  });
 
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "items",
+  });
 
-  const onSubmit =async (data)=>{
-  
-    const itemList ={
+  const onSubmit = async (data) => {
+    const itemList = {
       name: user?.displayName,
       email: user?.email,
       title: data.title,
       description: data.des,
-      totalMeal:data.total,
-      time:data.tag,
+      totalMeal: data.total,
+      time: data.tag,
       createdAt: new Date(),
-    }
-   
-   console.log(itemList);
+      items: data.items,
+    };
 
-  }
-
-
-
-
+    console.log(itemList);
+  };
 
   return (
     <div>
       <Helmet>
         <title>Mess Manage | Add Meal</title>
       </Helmet>
-      <SectionTitle
-        heading="add an item"
-        subHeading="What's new?"
-      ></SectionTitle>
+      <SectionTitle heading="add an item" subHeading="What's new?" />
       <div className="hero ">
         <div className="hero-content flex-col ">
-          
           <div className="card shadow-xl md:w-[700px]">
             <form onSubmit={handleSubmit(onSubmit)} className="card-body ">
               <div className="form-control">
@@ -70,55 +67,67 @@ const AddBazar = () => {
               </div>
 
 
-            
-            <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Item-1 (item name and weight)</span>
-                </label>
-                <input
-                  type="text"
-                  name="one"
-                  {...register("one", { required: true })}
-                  placeholder="Description"
-                  className="input input-bordered border-red-400"
-                />
-              </div>
-             
-         
-
-
-              
-
-
-
-
-
-              
-
-
-              
-
-
-
-
-
-             
-
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Meal Item</span>
+                  <span className="label-text">Meal Description</span>
                 </label>
                 <input
                   type="text"
-                  name="item"
-                  {...register("item", { required: true })}
-                  placeholder="Meal Item"
-                  className="input input-bordered border-green-300"
+                  name="des"
+                  {...register("des", { required: true })}
+                  placeholder="Description"
+                  className="textarea textarea-bordered h-24 border-red-400"
                 />
-               
               </div>
 
 
+
+
+              {fields.map((item, index) => (
+                <div key={item.id} className="form-control">
+                  <label className="label">
+                    <span className="label-text">{`Item-${index + 1} (item name and weight)`}</span>
+                  </label>
+
+
+                  <div className="flex  gap-6">
+                  <div>
+                  <input
+                    type="text"
+                    name={`items[${index}].one`}
+                    {...register(`items[${index}].one`, { required: true })}
+                    placeholder="Item Name"
+                    className="input input-bordered w-96 border-red-400"
+                  />
+                  </div>
+
+                  <div>
+                  <input
+                    type="text"
+                    name={`items[${index}].item`}
+                    {...register(`items[${index}].item`, { required: true })}
+                    placeholder="Weight"
+                    className="input input-bordered  border-red-400"
+                  />
+                  </div>
+                  </div>
+
+
+
+
+                  <button type="button" className="mt-2 btn bg-gray-200 hover:text-white hover:bg-black"  onClick={() => remove(index)}>
+                    Remove Item
+                  </button>
+                </div>
+              ))}
+
+              <button
+                type="button"
+                onClick={() => append({ one: "", item: "" })}
+                className="btn bg-blue-500 text-white hover:text-white hover:bg-black"
+              >
+                Add Item
+              </button>
 
               <div className="form-control">
                 <label className="label">
@@ -129,69 +138,46 @@ const AddBazar = () => {
                   name="total"
                   {...register("total", { required: true })}
                   placeholder="Total meal"
-                  className="input input-bordered border-green-300"
+                  className="input input-bordered border-red-400"
                 />
-               
               </div>
 
-            
-        {/* category */}
-    
-        {/* category */}
-        <div className="form-control mt-6  ">
-          <label className="label">
-            <span className="label-text">Meal Time</span>
-          </label>
-          <select 
-            defaultValue="default"
-            {...register("tag",{required:true})}
-            required
-            className="select select-bordered border-amber-400 w-full"
-          >
-            <option disabled value="default" >
-              Select a Tag
-            </option>
-            <option value="breakfast">Breakfast</option>
-            <option value="lunch">Lunch</option>
-            <option value="dinner">Dinner</option>
-            
-          </select>
-        </div>
-
-      
-
-              
-
-
-         
-
-
-
+              <div className="form-control mt-6  ">
+                <label className="label">
+                  <span className="label-text">Meal Time</span>
+                </label>
+                <select
+                  defaultValue="default"
+                  {...register("tag", { required: true })}
+                  required
+                  className="select select-bordered border-red-400 w-full"
+                >
+                  <option disabled value="default">
+                    Select a Tag
+                  </option>
+                  <option value="breakfast">Breakfast</option>
+                  <option value="lunch">Lunch</option>
+                  <option value="dinner">Dinner</option>
+                </select>
+              </div>
 
               <div className="form-control mt-6">
                 <input
-                  className="btn bg-green-300 hover:text-white hover:bg-black"
+                  className="btn bg-red-300 hover:text-white hover:bg-black"
                   type="submit"
                   value="Add Meal"
                 />
               </div>
             </form>
-          
-            <div>
-           
+            <div></div>
           </div>
-          </div>
-          
         </div>
       </div>
-           
-   
-<div>
 
-</div>
-
+      <div></div>
     </div>
   );
 };
 
 export default AddBazar;
+
