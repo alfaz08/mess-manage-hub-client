ae15acf5e7d5fb77e23aa756dadb06db9dd8ce9f
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-15 02:34:55 +0600

    add meal is updated

--- src/pages/Dashboard/Admin/AddMeal/AddMeal.jsx
+++ src/pages/Dashboard/Admin/AddMeal/AddMeal.jsx
@@ -1,25 +1,39 @@
-import { useForm } from "react-hook-form";
+import { Controller, useFieldArray, useForm } from "react-hook-form";
 import useAxiosPublic from "../../../../hooks/useAxiosPublic";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import useAuth from "../../../../hooks/useAuth";
 import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../SectionTitle/SectionTitle";
 import Swal from "sweetalert2";
-
-
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaPlusCircle } from "react-icons/fa";
+import { FaMinusCircle } from "react-icons/fa";
 
 const AddMeal = () => {
   const axiosPublic =useAxiosPublic()
   const axiosSecure = useAxiosSecure()
   const {user} =useAuth()
+  const [startDate, setStartDate] = useState(new Date());
 
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm();
+    control,
+  } = useForm({
+    defaultValues: {
+      items: [{  item: "" }],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "items",
+  });
 
 
   const onSubmit =async (data)=>{
@@ -32,6 +46,8 @@ const AddMeal = () => {
       totalMeal:data.total,
       time:data.tag,
       createdAt: new Date(),
+      date:data?.date ,
+      items: data.items,
     }
    
     const menuRes =await axiosSecure.post('/meals',mealItem)
@@ -78,7 +94,7 @@ const AddMeal = () => {
                   name="title"
                   {...register("title", { required: true })}
                   placeholder="Name"
-                  className="input input-bordered border-red-400"
+                  className="input input-bordered  border-red-400"
                 />
               </div>
 
@@ -92,7 +108,7 @@ const AddMeal = () => {
                   name="des"
                   {...register("des", { required: true })}
                   placeholder="Description"
-                  className="input input-bordered border-red-400"
+                  className="input input-bordered h-40 border-red-400"
                 />
               </div>
 
@@ -114,19 +130,49 @@ const AddMeal = () => {
 
              
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Meal Item</span>
-                </label>
-                <input
-                  type="text"
-                  name="item"
-                  {...register("item", { required: true })}
-                  placeholder="Meal Item"
-                  className="input input-bordered border-green-300"
-                />
-               
-              </div>
+              {fields.map((item, index) => (
+                <div key={item.id} className="form-control">
+                  <label className="label">
+                    <span className="label-text">{`Item-${
+                      index + 1
+                    } (Ingredients name and weight)`}</span>
+                  </label>
+
+                  <div className="mt-2 md:flex  gap-6">
+                    <div>
+                      <input
+                        type="text"
+                        name={`items[${index}].item`}
+                        {...register(`items[${index}].item`, { required: true })}
+                        placeholder="Ingredients Name"
+                        className="input input-bordered w-72 border-red-400"
+                      />
+                    </div>
+
+                   
+                    <div>
+                    <button
+                    type="button"
+                    className=" btn bg-red-200 hover:text-white hover:bg-black"
+                    onClick={() => remove(index)}
+                  >
+                  <FaMinusCircle className="text-2xl" />
+                  </button>
+                    </div>
+                  </div>
+
+                  
+                </div>
+              ))}
+
+              <button
+                type="button"
+                onClick={() => append({ item: "" })}
+                className="btn w-24 bg-red-200  hover:text-white hover:bg-black"
+              >
+                <FaPlusCircle className="text-2xl"/>
+
+              </button>
 
 
 
@@ -139,7 +185,7 @@ const AddMeal = () => {
                   name="total"
                   {...register("total", { required: true })}
                   placeholder="Total meal"
-                  className="input input-bordered border-green-300"
+                  className="input input-bordered border-red-400"
                 />
                
               </div>
@@ -156,7 +202,7 @@ const AddMeal = () => {
             defaultValue="default"
             {...register("tag",{required:true})}
             required
-            className="select select-bordered border-amber-400 w-full"
+            className="select select-bordered border-red-400 w-full"
           >
             <option disabled value="default" >
               Select a Tag
@@ -168,7 +214,38 @@ const AddMeal = () => {
           </select>
         </div>
 
-      
+        <div className="form-control ">
+  <label className="label">
+    <span className="label-text">Select Bazar Date</span>
+  </label>
+  <Controller
+    name="date"
+    className="border-red-400 border"
+    control={control}
+    render={({ field }) => (
+      <div className="relative">
+        <DatePicker
+          selected={field.value}
+          onChange={(date) => {
+            field.onChange(date);
+            setStartDate(date);
+          }}
+          minDate={new Date()} // Set the minimum date to today
+          customInput={
+            <input
+              className="w-full p-2 border-red-400 border rounded-md"
+              placeholder="Select date"
+            />
+          }
+          required
+        />
+        <div className="absolute inset-y-0 cursor-pointer  flex items-center px-2 pointer-events-none">
+          <FaCalendarAlt className="text-red-400" />
+        </div>
+      </div>
+    )}
+  />
+</div>
 
               
 
@@ -180,7 +257,7 @@ const AddMeal = () => {
 
               <div className="form-control mt-6">
                 <input
-                  className="btn bg-green-300 hover:text-white hover:bg-black"
+                  className="btn bg-red-300 hover:text-white hover:bg-black"
                   type="submit"
                   value="Add Meal"
                 />
