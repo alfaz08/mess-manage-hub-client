32ecd2268561d5333b8139e3d379c978f5ecdf29
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-18 13:55:21 +0600

    z value increase for show dropdown in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
           }
         </div>
       </div>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="mt-3 z-[2] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
         <li>
         {user.displayName ?
            user.displayName
