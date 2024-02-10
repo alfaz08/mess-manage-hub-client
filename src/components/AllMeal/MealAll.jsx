5296d53c44d195b59c8759798cb0c106efe46240
Alfaz Hossain <alfaz.cse@gmail.com>
2024-02-10 19:04:39 +0600

    sort is added in home page

--- src/components/AllMeal/MealAll.jsx
+++ src/components/AllMeal/MealAll.jsx
@@ -3,8 +3,7 @@ import useMeal from "../../hooks/useMeal";
 
 const MealAll = () => {
   const [allMeal] = useMeal();
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 6;
+  
 
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth() + 1;
@@ -13,6 +12,20 @@ const MealAll = () => {
     const totalMeal = new Date(item.date);
     return totalMeal.getMonth() + 1 === currentMonth;
   });
+ 
+const [sortMeal, setSortMeal] = useState("");
+
+  const handleSort= (event)=>{
+    const selectedValue = event.target.value;
+    console.log("Selected value:", selectedValue);
+    const sortedMeal = singleMealThisMonth.filter(item=>item.time===selectedValue)
+    setSortMeal(sortedMeal)
+  }
+  console.log(sortMeal);
+
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
   // Calculate the index range for the current page
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -24,6 +37,8 @@ const MealAll = () => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+
+
   return (
     <div>
       <div className="grid justify-center mt-10">
@@ -31,8 +46,75 @@ const MealAll = () => {
           All Meal This Month
         </h2>
       </div>
-      Sort by breakfast
-      <div className="grid grid-cols-1 md:grid-cols-3 ">
+      <div>
+
+
+      <div>
+          <div className="flex mr-4 justify-end">
+            <select
+            className="border border-red-500 rounded px-4 py-2 mt-2"
+              name="sortBy"
+              id="sortBy"
+             onChange={handleSort}
+            >
+              <option value="">
+                {" "}
+               
+                sort
+                
+               
+              </option>
+              <option value="breakfast">Breakfast</option>
+              <option value="lunch">Lunch</option>
+              <option value="dinner">Dinner</option>
+             
+            </select>
+          </div>
+        </div>
+   
+
+
+
+
+
+
+      </div>
+
+
+      {
+        sortMeal ? 
+        <div className="grid grid-cols-1 md:grid-cols-3 ">
+        {sortMeal?.map((item) => (
+          <div key={item._id} className="card w-96 mt-2 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{item.title}</h2>
+              <p>Total Meal: {item.totalMeal}</p>
+              <p> Meal Time: {item.time}</p>
+              <p>
+                Date:{" "}
+                {item.date &&
+                  new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    day: "numeric",
+                    month: "long",
+                  }).format(new Date(item.date))}
+              </p>
+              <div className="card-actions">
+                <button className="btn bg-red-300">See Details</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+        :
+<div className="grid grid-cols-1 md:grid-cols-3 ">
         {currentItems?.map((item) => (
           <div key={item._id} className="card w-96 mt-2 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
@@ -62,7 +144,15 @@ const MealAll = () => {
           </div>
         ))}
       </div>
-      <div className="flex justify-center mt-4">
+      }
+
+
+
+
+      {
+        sortMeal ?
+        null:
+        <div className="flex justify-center mt-4">
   {[...Array(Math.ceil(singleMealThisMonth?.length / itemsPerPage))].map(
     (_, index) => (
       <div
@@ -77,6 +167,7 @@ const MealAll = () => {
     )
   )}
 </div>
+      }
     </div>
   );
 };
